c93f51c39e507d4d91a3e334171adb4d5f393635
Joseph <frasier.joseph@gmail.com>
2023-05-08 14:34:14 -0500

    answer to weather to app

--- fetch.js
+++ fetch.js
@@ -1,14 +1,17 @@
+const findWeather = document.querySelector("#findweather");
+findWeather.addEventListener("click", getPokemon);
 async function getPokemon() {
-  const pokemon = await fetch("https://pokeapi.co/api/v2/pokemon");
-  const json = await pokemon.json();
+  const pokemon = await fetch(
+    "https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=29.7604&lon=-95.3698&appid=ff1cadec65d9f10c3aafc2a01b14612c"
+  );
   const body = document.querySelector("body");
-  for (const poke of json.results) {
-    const name = document.createElement("h1");
-    const url = document.createElement("a");
-    name.innerHTML = poke.name;
-    url.innerHTML = poke.name;
-    url.href = poke.url;
-    body.append(name, url);
-  }
+
+  const json = await pokemon.json();
+  console.log(json);
+  const name = document.createElement("h1");
+  const temp = document.createElement("h1");
+  name.innerHTML = json.name;
+  temp.innerHTML = json.main.temp + " f";
+  body.append(name, temp);
+  console.log(name);
 }
-getPokemon();
